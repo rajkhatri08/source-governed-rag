d49f4b59c2f9f987c8823570db8257c88c8e0c74
Raj Khatri <rajkhatrigbpuat@gmail.com>
2026-08-23 20:51:36 +0530

    remove sentence-transformers; use ChromaDB built-in embeddings to fit deployment memory

--- retrieve.py
+++ retrieve.py
@@ -1,7 +1,5 @@
 import chromadb
-from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def build_index(chunks, metadatas):
     client = chromadb.Client()
@@ -11,17 +9,14 @@ def build_index(chunks, metadatas):
         pass
     collection = client.create_collection("gdpr")
 
-    vectors = model.encode(chunks).tolist()
-
     ids = []
     for i in range(len(chunks)):
         ids.append("chunk_" + str(i))
 
-    collection.add(documents=chunks, embeddings=vectors, ids=ids, metadatas=metadatas)
+    collection.add(documents=chunks, metadatas=metadatas, ids=ids)
     return collection
 
 
 def search(collection, question, n):
-    query_vector = model.encode(question).tolist()
-    results = collection.query(query_embeddings=[query_vector], n_results=n)
+    results = collection.query(query_texts=[question], n_results=n)
     return results["documents"][0], results["ids"][0], results["distances"][0], results["metadatas"][0]
